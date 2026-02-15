654ac6f0c18ef6afca45fc728ab026a3c24ac9ff
Devashish2112 <devashishramola@gmail.com>
2026-02-15 17:02:03 +0530

    Reapply "Implement the websocket server"
    This reverts commit 0a49268501217061f9ece3ca3905338177c9cb10.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import express from "express";
 import { matchRouter } from "./routes/matches.js";
+import http from 'http';
+import { attachWebSocketServer } from "./ws/server.js";
 
 const app = express();
+const server = http.createServer(app);
+ 
 const PORT = process.env.PORT || 8000;
+const HOST = process.env.HOST || '0.0.0.0';
 
 // JSON middleware
 app.use(express.json());
@@ -14,7 +19,13 @@ app.get("/", (req, res) => {
 
 app.use("/matches", matchRouter);
 
+const { broadcastMatchCreated } = attachWebSocketServer(server);
+app.locals.broadcastMatchCreated = broadcastMatchCreated;
+
 // Start server
-app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
+server.listen(PORT, HOST, () => {
+  const baseUrl = HOST === '0.0.0.0' ? `http://localhost:${PORT}` : `http://${HOST}:${PORT}`;
+
+  console.log(`Server is running on ${baseUrl}`);
+  console.log(`Websocket Server is running on ${baseUrl.replace('http', 'ws')}/ws`); 
 });
